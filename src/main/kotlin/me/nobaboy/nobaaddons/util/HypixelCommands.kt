213da3c8e2783c2fdfe02670078b2ba60108ee71
nobaboy <84668101+nobaboy@users.noreply.github.com>
2024-05-04 19:56:53 +0300

    Added HypixelCommands to simplify sending hypixel commands

---
+++ src/main/kotlin/me/nobaboy/nobaaddons/util/HypixelCommands.kt
@@ -0,0 +1,69 @@
+package me.nobaboy.nobaaddons.util
+
+object HypixelCommands {
+
+    // Messaging Commands
+    fun privateChat(player: String, message: String) {
+        send("msg $player $message")
+    }
+
+    fun guildChat(message: String) {
+        send("gc $message")
+    }
+
+    fun partyChat(message: String) {
+        send("pc $message")
+    }
+
+    // Inventory Related Commands
+    /**
+     * Retreives items from the sacks with the specified item ID obtained from wiki.hypixel.net
+     *
+     * @param itemID The internal ID that's obtained from the wiki
+     */
+    fun getFromSacks(itemID: String, amount: Int) {
+        send("gfs $itemID $amount")
+    }
+
+    // Party Related Commands
+    fun partyInvite(player: String) {
+        send("party invite $player")
+    }
+
+    fun partyJoin(player: String) {
+        send("party join $player")
+    }
+
+    fun partyLeave() {
+        send("party leave")
+    }
+
+    fun partyDisband() {
+        send("party disband")
+    }
+
+    fun partyKick(player: String) {
+        send("party kick $player")
+    }
+
+    fun partyTransfer(player: String) {
+        send("party transfer $player")
+    }
+
+    fun partyAllInvite() {
+        send("party settings allinvite")
+    }
+
+    fun partyWarp() {
+        send("party warp")
+    }
+
+    fun partyList() {
+        send("party list")
+    }
+
+    // Helper function to send commands through queue system.
+    private fun send(command: String) {
+        ChatUtils.queueCommand(command)
+    }
+}
